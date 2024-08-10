cf953770f67a7048a4a3229f0a631424662b2aeb
abdul <abdulabba03@gmail.com>
2024-08-10 21:36:13 +0100

    Python lsit save 2

--- python_list.py
+++ python_list.py
@@ -33,3 +33,28 @@ print(student_names[len(student_names) - 1])
 # to check the number of items in a list in reverse order as -1 for the last item in the list
 print(student_names[len(student_names) - 2])
 
+#checking the data type of the list
+print(type(student_names))
+
+# We can have an empty list
+empty_class = []
+print("The empty list ; ", empty_class)
+print("the lenght of the list : ", len(empty_class))
+print("the datayoe will return list : ", type(empty_class))
+
+#using the list contructor
+range_list = range(1, 10)
+print("range data type will be : ", type(range_list))
+print(range_list)
+print(list(range(1, 20)))
+
+# converting string to list
+poem = "Today is Great"
+print(list(poem))
+
+# converting tuple to list
+Height = (3, 4, 5)
+print(Height)
+print("Tuple type : ", type(Height))
+print(list(Height))
+

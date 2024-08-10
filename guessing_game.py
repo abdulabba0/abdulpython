76a6f34a13d1ad950ad71c38f267641c664e7395
abdul <abdulabba03@gmail.com>
2024-08-10 20:22:03 +0100

    Guessing game Save

---
+++ guessing_game.py
@@ -0,0 +1,26 @@
+"""
+1. Understand and interprete the problem
+2. Identify the input
+3. Identify the output
+4. Develop method to the problem
+5. Write pseudocode to solve the problem
+6. Write code to solve the problem
+
+
+1. The is to create a guessing game where user will be presented with a list of number and will be asked to guess which of the number the computer has guessed
+
+2. The guessed number from the user
+
+3. The output should any or all of
+a. True or false if the user guessed right
+b. Display the computer guess
+c. Display the user guess
+d. The number of guess attempt left for the user have
+e. Display the list of number to guess from
+
+4.
+- we use random module to generate a random number from 1-9, that will be the computer guess
+We take the computer guess and list of numbers including the answer
+- We run a while loop
+- We tell the the user to guess a number between 1-9
+"""
